586f4428930ded531ed34e62dd4f0451559c6078
josegargestion <contacto@gestionvirtual.com.es>
2022-09-28 07:35:55 +0200

    hjkghj
    ghjk

--- build/core/spiffs/spiffs_hydrogen.cpp.d
+++
@@ -1,5 +0,0 @@
-c:\Users\usuario\Documents\GitHub\Inver_ota\build\core\spiffs\spiffs_hydrogen.cpp.o: \
- C:\Users\usuario\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\spiffs\spiffs_hydrogen.cpp \
- C:\Users\usuario\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\spiffs\spiffs.h \
- C:\Users\usuario\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\spiffs\spiffs_config.h \
- C:\Users\usuario\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.0.2\cores\esp8266\spiffs\spiffs_nucleus.h
